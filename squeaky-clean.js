5edd8d928eee6b9aa8026fb814490b3b0410f785
Jason Studstill <standstill64@gmail.com>
2015-09-04 20:56:57 -0700

    Implement squeaky-clean and find-something-in-an-array

---
+++ squeaky-clean.js
@@ -0,0 +1,19 @@
+// Write a solution to cleanup arrays. This can exisit entirely in the
+// squeakyClean function or contain multiple helper functions.
+
+// Your squeakyClean function should accept an input array of values and
+// return a new array with all empty strings, 0, null and undefined removed.
+
+// Example:
+
+// var originalArray = ['click1','click2',null,'','','submitForm'];
+// the solution you write should return this:
+
+// var cleanedArray = ['click1', 'click2','submitForm']
+
+// Essentially only return values that are truthy
+
+// Solution:
+function squeakyClean(arr) {
+  return arr.filter(function(e) {return (e)});
+}
